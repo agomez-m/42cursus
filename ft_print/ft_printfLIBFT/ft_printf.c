73ffd1eb6bf21e9ac5e9044b9494036e3f4c2d53
agomez-m <arturo.bwh@gmail.com>
2023-09-20 23:37:40 +0200

    act printf

--- ft_print/ft_printfLIBFT/ft_printf.c
+++ ft_print/ft_printfLIBFT/ft_printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "libft.h"
-#include <unistd.h>
-#include <stdlib.h>
 
 int print_char(int c)
 {
-    write(1, &c, 1);
+    write(1, c, 1);
     return (1);
 }
 
